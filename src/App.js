a2353386a10e1e8d60a61f84d3de3b74fd599863
Marco Galindo <marco1408@gmail.com>
2021-07-21 18:40:54 -0300

    exercício 2 finalizado

--- src/App.js
+++ src/App.js
@@ -2,14 +2,48 @@ import React from 'react';
 import TrafficSignal from './TrafficSignal';
 import './App.css';
 import Cars from './Cars';
+import MyContext from './MyContext';
 
-function App() {
-  return (
-    <div className="container">
-      <Cars />
-      <TrafficSignal />
-    </div>
-  );
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      signalColor: 'green',
+      red: false,
+      blue: false,
+      yellow: false,
+    };
+    this.changeSignal = this.changeSignal.bind(this);
+    this.moveCar = this.moveCar.bind(this);
+  }
+
+  changeSignal(color) {
+    this.setState({
+      signalColor: color,
+    });
+  }
+
+  moveCar(car, side) {
+    this.setState({
+      [car]: side,
+    })
+  }
+
+  render() {
+    const value = {
+      signalColor: this.state.signalColor,
+      redCar: this.state.red,
+      blueCar: this.state.blue,
+      yellowCar: this.state.yellow,
+      moveCar: this.moveCar,
+      changeSignal: this.changeSignal,
+    }
+  
+    return (
+      <MyContext.Provider value={value}>
+        <Cars />
+        <TrafficSignal />
+      </MyContext.Provider>
+    );
+  }
 }
-
-export default App;
