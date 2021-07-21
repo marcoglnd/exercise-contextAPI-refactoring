6aeb02f159606042682af22860e45bac37cbe498
Marco Galindo <marco1408@gmail.com>
2021-07-21 17:10:49 -0300

    exercicio 1 finalizado

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Cars from './Cars';
+import MyContext from './MyContext';
 
-function App() {
-  return (
-    <Cars />
-  );
+export default class App extends Component{
+  constructor() {
+    super();
+    this.state = {
+      red: false,
+      blue: false,
+      yellow: false,
+    }
+    this.moveCar = this.moveCar.bind(this);
+  }
+
+  moveCar(car, side) {
+    this.setState({
+      [car]: side,
+    });
+  }
+
+  render() {
+    const contextValue = {
+      redCar: this.state.red,
+      blueCar: this.state.blue,
+      yellowCar: this.state.yellow,
+      moveCar: this.moveCar,
+    }
+    return (
+      <MyContext.Provider value={ contextValue }>
+        <Cars />
+      </MyContext.Provider>
+    );
+  }
 }
-
-export default App;
